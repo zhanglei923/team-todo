8131e948b18d7076811fbda4023add6e81e0f080
zhanglei <zhang.lei.923@gmail.com>
2018-07-11 03:14:21 +0800

    up

--- project/src/TodoList/item/TitleInput.js
+++ project/src/TodoList/item/TitleInput.js
@@ -3,11 +3,17 @@ import React, { Component } from 'react';
 class StatusSelector extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isEditing: false
+        }
       }
   render() {
     const task = this.props.task;
     return (
-        <input placeholder="Enter task description here..." value={task.title} onChange={(e) => this.props.onChange(e, task.id)}/>
+        this.state.isEditing ?
+        <input className={"editing-title"} placeholder="Enter task description here..." value={task.title} onChange={(e) => this.props.onChange(e, task.id)}/>
+        :
+        <span className={"readonly-title"}>{task.title?task.title:"Blank Title..."}</span>
     );
   }
 }
