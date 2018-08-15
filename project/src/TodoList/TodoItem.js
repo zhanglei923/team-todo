d8ac5a0a5185158ec77f6da3d9003c968bfdfd33
zhanglei <zhang.lei.923@gmail.com>
2018-08-16 01:24:32 +0800

    up

--- project/src/TodoList/TodoItem.js
+++ project/src/TodoList/TodoItem.js
@@ -60,15 +60,14 @@ class TodoList extends Component {
       {(task.isGroup)?
         <tr key={"group"+i} className={"groupName"}>
           <td colSpan={100}>
-            <div style={{float:'left'}}>
+            <div style={{float:'left',padding:'2px 0 0 2px'}}>
               {task.groupName}
             </div>
-            <div style={{marginRight: '30px', float:'left'}}>
-              <a href="javascript:void(0)" onClick={(e)=>this.props.handleMoveUp(task.id)} ><Icon size={9} icon={arrowUp} /></a>
-              <a href="javascript:void(0)" onClick={(e)=>this.props.handleMoveDown(task.id)} ><Icon size={9} icon={arrowDown} /></a>        
-              &nbsp;
-              <a href="javascript:void(0)" onClick={(e)=>this.props.handleGroupRename(task.id)} ><Icon size={12} icon={pencil} /></a>  
-              <a href="javascript:void(0)" onClick={(e)=>this.props.handleGroupRemove(task.id)} ><Icon size={12} icon={close} /></a>    
+            <div style={{marginLeft: '22px', float:'left',padding:'2px 0 0 2px'}}>
+              <a href="javascript:void(0)" style={{marginLeft:'5px'}} onClick={(e)=>this.props.handleMoveUp(task.id)} ><Icon size={9} icon={arrowUp} /></a>
+              <a href="javascript:void(0)" style={{marginLeft:'5px'}} onClick={(e)=>this.props.handleMoveDown(task.id)} ><Icon size={9} icon={arrowDown} /></a>
+              <a href="javascript:void(0)" style={{marginLeft:'5px'}} onClick={(e)=>this.props.handleGroupRename(task.id)} ><Icon size={12} icon={pencil} /></a>  
+              <a href="javascript:void(0)" style={{marginLeft:'5px'}} onClick={(e)=>this.props.handleGroupRemove(task.id)} ><Icon size={12} icon={close} /></a>    
             </div>      
           </td>
         </tr>
