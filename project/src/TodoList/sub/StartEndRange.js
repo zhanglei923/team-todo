2b66a030d47214ba56fc397f9094c91bba190a88
zhanglei <zhanglei@xiaoshouyi.com>
2018-08-17 18:00:06 +0800

    up

--- project/src/TodoList/sub/StartEndRange.js
+++ project/src/TodoList/sub/StartEndRange.js
@@ -28,12 +28,20 @@ class StartEndRange extends Component {
         dm2 = moment()
         daysLeft = dateUtil.getTimeSpan(dm1, dm2, 'end')
     }
-
+    let daysLeftNum = daysLeft ? parseInt(daysLeft) : 0
     return (
         <div>
-            {daysLost ? <div className="daysLost">{daysLost}</div>:false}
-            {daysLeft ? <div className="daysLeft">{!/^-/.test(daysLeft)?daysLeft:'0'}</div>:false}
-            {daysRange ? <div className="daysRange">{daysRange}</div>:false}
+            {daysLost ?(task.status === 'done' ? false : ( <div className="daysLost">{daysLost}</div>)):false}
+            {daysLeft?(task.status === 'done' ? false : (daysLeftNum < 0) ?
+                <div className="daysLeft out-of-time">{daysLeft}</div>
+                :
+                <div className="daysLeft">{daysLeft}</div>
+            ):false}
+            {daysRange?(task.status === 'done' ? 
+                        (<div className="daysRange">{daysRange}d Cost</div>) 
+                        : 
+                        <div className="daysRange">{daysRange}</div>
+            ):false}
         </div>
     );
   }
