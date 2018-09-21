3754e375eb40bc29e5b818784149188e2da79fd2
zhanglei <zhang.lei.923@gmail.com>
2018-09-22 03:01:17 +0800

    up

--- project/src/index.js
+++ project/src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import messenger from 'longbow-messenger.js'
 
+messenger.listen('ask_for_sum', (num1, num2)=>{
+    return num1 + num2;
+})
+messenger.emit('ask_for_sum', 1, 2).onResponse((result, msgInfo)=>{    
+    console.log('sum:', result);
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
